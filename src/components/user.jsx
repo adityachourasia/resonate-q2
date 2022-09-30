45887702ea216b0a1f3233502f341e1740513f30
adityachourasia <adityabsd@gmail.com>
2022-09-30 07:42:17 +0530

    first commit

---
+++ src/components/user.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state.user = []
+    }
+    state = { user: [] }
+    
+    render() {
+        return (
+            // <React.Fragment>
+                <tr>
+                    <td>{this.props.userInfo.id}</td>
+                    <td>{this.props.userInfo.name}</td>
+                    <td>{this.props.userInfo.username}</td>
+                    <td>{this.props.userInfo.email}</td>
+                    <td>{this.props.userInfo.phone}</td>
+                    <td>{this.props.userInfo.website}</td>
+                    <td>{this.props.userInfo.address.city}</td>
+                    <td>{this.props.userInfo.company.name}</td>
+                </tr>
+            // </React.Fragment>
+        );
+    }
+}
+
+export default User;
+
+
+// userInfo=
+// {
+//     "id": "",
+//     "name": "",
+//     "username": "",
+//     "email": "",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+//   }
---
+++ src/components/users.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import User from "./user"
+import Table from 'react-bootstrap/Table';
+
+class Users extends Component {
+
+    state = {
+        usersInfo: []
+    }
+
+
+
+    componentDidMount() {
+        const url = "https://jsonplaceholder.typicode.com/users"
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ usersInfo: data })
+                console.log("From Users", data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+
+                <h1>Contact Details</h1>
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            <th>Website</th>
+                            <th>Address/City</th>
+                            <th>Company Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                {
+                    this.state.usersInfo.map((user) => {
+                        // console.log("here",user.id)
+                        return <User key={user.id} userInfo={user} ></User>
+                    })
+                }
+                </tbody>
+                </Table>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Users;
+
+
+// userInfo=
+// {
+//     "id": "",
+//     "name": "",
+//     "username": "",
+//     "email": "",
+//     "address": {
+//       "street": "Kulas Light",
+//       "suite": "Apt. 556",
+//       "city": "Gwenborough",
+//       "zipcode": "92998-3874",
+//       "geo": {
+//         "lat": "-37.3159",
+//         "lng": "81.1496"
+//       }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//       "name": "Romaguera-Crona",
+//       "catchPhrase": "Multi-layered client-server neural-net",
+//       "bs": "harness real-time e-markets"
+//     }
+//   }
